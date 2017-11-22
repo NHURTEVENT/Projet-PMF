8fce4288ac60c210e43759caecb20f06e2970115
MazurierPierre <pierre.mazurier@viacesi.fr>
2017-11-22 15:01:10 +0100

    Modified iView

--- src/View/iView.java
+++ src/View/iView.java
@@ -1,7 +1,19 @@
 package View;
 
-import java.util.Observer;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
-public interface iView extends Observer {
+public interface iView {
 
+	JLabel tempInt = null;
+	JLabel tempExt = null;
+	JLabel tempPeltier = null;
+	JLabel tauxHumi = null;
+	JTextField tempConsigne = null;
+	JButton btnConfirmer = null;
+	
+	public void switchDoorIcon(int i);
+	public void switchHumiIcon(int i);
+	
 }
